92f88c834b9cbe4c1067a86ae57450b3686b5796
root <root@bmcho.localdomain>
2022-09-02 23:01:54 +0900

    initialized project

---
+++ src/test/java/com/bmcho/springsecuritypractice/SpringsecuritypracticeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bmcho.springsecuritypractice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringsecuritypracticeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
